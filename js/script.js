3a98de8101f3478b401a14df21e24d318ae56885
V.Zaionchkovskiy <nechtsoft@mail.ru>
2019-10-31 08:54:43 +0300

    cons fixed - js and css

--- js/script.js
+++ js/script.js
@@ -39,6 +39,8 @@ close.addEventListener('click', function(evt){
 form.addEventListener('submit', function (evt) {
   if (!nameForm.value || !emailForm.value) {
     evt.preventDefault();
+    popup.classList.remove('modal-error');
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add('modal-error');
   } else {
     if (isStorageSupport) {
